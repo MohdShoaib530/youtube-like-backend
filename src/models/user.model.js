b36d573bf554bb10b803e47ea372590f4efba19e
MohdShoaib530 <mohdshoaib91530@gmail.com>
2024-02-18 00:46:44 +0530

    access and refresh token created

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -56,9 +56,36 @@ userSchema.pre("save", async function (next){
     next()
 })
 
-userSchema.method.isPasswordCorrct = async function (password){
+userSchema.methods.isPasswordCorrct = async function (password){
+    await bcrypt.compare(password,this.password)
+}
 
-   return await bcrypt.compare(password,this.password)
+userSchema.methods.generateAccessToken = function (){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+userSchema.methods.generateRefreshToken = function (){
+    return jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
 }
 
 export default mongoose.model("User",userSchema);
