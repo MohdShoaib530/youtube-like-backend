165029aafe1fdbfe5ab785f5b340b351511903fb
MohdShoaib530 <mohdshoaib91530@gmail.com>
2024-03-09 12:05:56 +0530

    cloudinary setup complete

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -14,3 +14,38 @@ const storage = multer.diskStorage({
 export const upload = multer({
     storage
 });
+
+
+
+/* eslint-disable no-console */
+/* import multer from 'multer';
+import path from 'path';
+
+const upload = multer({
+    dest: './public/temp',
+    limits: { fileSize: 50 * 1024 * 1024 }, // 50 mb in size max limit
+    storage: multer.diskStorage({
+        destination: './public/temp',
+        filename: (_req, file, cb) => {
+            cb(null, file.originalname);
+        }
+    }),
+    fileFilter: (_req, file, cb) => {
+        const ext = path.extname(file.originalname);
+
+        if (
+            ext !== '.jpg' &&
+            ext !== '.jpeg' &&
+            ext !== '.webp' &&
+            ext !== '.png' &&
+            ext !== '.mp4'
+        ) {
+            cb(new Error(`Unsupported file type! ${ext}`), false);
+            return;
+        }
+
+        cb(null, true);
+    }
+});
+
+export default upload; */
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-console */
+import { v2 as cloudinary, v2 } from 'cloudinary';
+import fs from 'fs';
+
+// Cloudinary configuration
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async function(localFilePath){
+    try {
+        if(!localFilePath) {
+            return null;
+        }
+
+        const response = await v2.uploader.upload(localFilePath, {
+            resource_type: 'auto',
+            folder: 'youtube', // Save files in a folder named lms
+            width: 250,
+            height: 250,
+            gravity: 'faces', // This option tells cloudinary to center the image around detected faces (if any) after cropping or resizing the original image
+            crop: 'fill'
+        });
+
+        console.log('file uploaded on cloudinary', response.url);
+
+        fs.unlinkSync(localFilePath);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath);
+        return null;
+    }
+};
+
+export default uploadOnCloudinary;
