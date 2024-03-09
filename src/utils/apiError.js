ebabb6dfa73cc67b1337c520dc43b1a45068101a
MohdShoaib530 <mohdshoaib91530@gmail.com>
2024-03-09 10:59:00 +0530

    apiError setup

---
+++ src/utils/apiError.js
@@ -0,0 +1,22 @@
+class apiError extends Error {
+    constructor(
+        statusCode,
+        message = 'something went wrong',
+        errors = [],
+        stack = ''
+    ){
+        super(message);
+        this.statusCode = statusCode;
+        this.errors = errors;
+        this.data = null;
+        this.success = false;
+
+        if(stack){
+            this.stack = stack;
+        } else {
+            Error.captureStackTrace(this,this.constructor);
+        }
+    }
+}
+
+export default apiError;
