33e36975ddbe616b2db3e43b281615d1d90e6084
MohdShoaib530 <mohdshoaib91530@gmail.com>
2024-03-09 11:27:46 +0530

    asychHandler setup

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,35 @@
+asyncHandler = (reqHandler) => {
+    return async (req, res, next ) => {
+        try {
+            await reqHandler(req, res, next);
+        } catch (error) {
+            next(error);
+        }
+    };
+};
+
+export default asyncHandler;
+
+// const asyncHandler = async (reqHandlerFun) => {
+//     return (req,res,next) => {
+//         Promise.resolve(reqHandlerFun(req,res,next))
+//                .catch((error) => next(error))
+//     }
+// };
+
+// export default asyncHandler;
+
+// const asyncHandler = async () => {}
+// const asyncHandler = (fn) => () => {}
+// const asyncHandler = (fn) => async () => {}
+
+// const asyncHandler = (fn) => async(req,res,next) => {
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
