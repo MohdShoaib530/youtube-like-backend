91f073235cc55c2a87c5eecfde2257633076bf54
MohdShoaib530 <mohdshoaib91530@gmail.com>
2024-03-09 11:21:08 +0530

    apiResponse setup

---
+++ src/utils/apiResponse.js
@@ -0,0 +1,15 @@
+class apiResponse {
+    constructor(
+        statusCode,
+        data,
+        message = 'success',
+        success = true
+    ){
+        this.statusCode = statusCode;
+        this.data = data;
+        this.message = message;
+        this.success = success;
+    }
+}
+
+export default apiResponse;
