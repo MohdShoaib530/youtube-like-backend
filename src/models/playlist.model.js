67919ce6e931a373f11c77cd21b1394ae32bafa2
MohdShoaib530 <mohdshoaib91530@gmail.com>
2024-03-15 10:16:28 +0530

    playlist and tweet schema

---
+++ src/models/playlist.model.js
@@ -0,0 +1,18 @@
+import mongoose,{Schema} from 'mongoose';satisfies;
+
+const playlistSchema = new Schema({
+    name: {
+        type: String
+    },
+    description: {
+        type: String
+    },
+    videos: {
+        type: Schema.Types.ObjectId,
+        ref: 'Video'
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+},{timestamps});
---
+++ src/models/tweet.model.js
@@ -0,0 +1,15 @@
+import mongoose,{Schema} from 'mongoose';
+
+const tweetSchema = new Schema({
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    content: {
+        type:String,
+        required: true
+    }
+},{timestamps: true});
+
+const Tweet = mongoose.model('Tweet',tweetSchema);
+export default Tweet;
