bc35274814eba0b7be448a69ea99172b16adc8dd
MohdShoaib530 <mohdshoaib91530@gmail.com>
2024-03-17 22:52:19 +0530

    playlist router is working

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -96,13 +96,71 @@ const deletePlaylist = asyncHandler(async (req, res, next) => {
 
 });
 const addVideoToPlaylist = asyncHandler(async (req, res, next) => {
+    const { videoId ,playlistId } = req.params;
+    console.log(videoId,playlistId);
 
+    if(!playlistId || !playlistId){
+        throw next(new apiError(400,'playListId is required ot delete the playlist'));
+    }
+
+    const addVideo = await Playlist.findByIdAndUpdate(
+        playlistId,
+        {
+            $push: { videos: { $each: [videoId] } }
+        },
+        {
+            new: true
+        }
+    );
+    console.log('delete',addVideo);
+
+    if(!addVideo){
+        throw next(new apiError(500,'something went wrong while updating video'));
+    }
+
+    res
+        .status(200)
+        .json(new apiResponse(200,addVideo,'playlist has been updated'));
 });
 const removeVideoFromPlaylist = asyncHandler(async (req, res, next) => {
+    const { videoId ,playlistId } = req.params;
 
+    if(!playlistId || !playlistId){
+        throw next(new apiError(400,'playListId is required ot delete the playlist'));
+    }
+
+    const removeVideo = await Playlist.findByIdAndUpdate(
+        playlistId,
+        {
+            $pull: { videos: videoId }
+        },
+        {
+            new: true
+        }
+    );
+    console.log('delete',removeVideo);
+
+    if(!removeVideo){
+        throw next(new apiError(500,'something went wrong while updating video'));
+    }
+
+    res
+        .status(200)
+        .json(new apiResponse(200,removeVideo,'playlist has been updated'));
 });
 const getUserPlaylists = asyncHandler(async (req, res, next) => {
 
+    const userPlaylist = await Playlist.find({owner: req.user?._id}).populate('videos');
+
+    console.log('use',userPlaylist);
+
+    if(!userPlaylist){
+        throw next(new apiError(500,'something went wrong whle getting playlist'));
+    }
+
+    res
+        .status(200)
+        .json(new apiResponse(200,userPlaylist,'user playlist has been fetched successfully'));
 });
 
 export {
