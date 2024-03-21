a1ab5b5c1beaf5bb664b02bb16b92160a454a561
MohdShoaib530 <mohdshoaib91530@gmail.com>
2024-03-21 05:33:17 +0530

    healthDetails router is working

---
+++ src/controllers/healthcheck.controller.js
@@ -0,0 +1,12 @@
+import {asyncHandler} from '../utils/asyncHandler.js';
+
+
+const healthcheck = asyncHandler(async (req, res) => {
+    res
+        .status(200)
+        .json(200,'Health details of the user are here');
+});
+
+export {
+    healthcheck
+};
